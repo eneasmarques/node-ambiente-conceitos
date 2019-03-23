efeab6fcd554dd5b5d3dcf71adb724b8603a599b
eneasmarques <eneasmarques@outlook.com.br>
2019-03-23 08:02:35 -0300

    feature: Adicionado Middleware para todas as rotas

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ const express = require('express');
 
 const app = express();
 
+const logMiddleware = (req, res, next) => {
+  console.log(`URL: ${req.url}, Method: ${req.method}`);
+
+  req.nameSystem = 'Aplicativo X';
+
+  // usando next() para não parar o fluxo
+  return next();
+};
+
+// será chamado em todas as rotas
+app.use(logMiddleware);
+
 app.get('/', (req, res) => {
-  return res.send('Hello World');
+  return res.send(`Home ${req.nameSystem}`);
 });
 
 // url params /nome_param
 app.get('/nome/:name', (req, res) => {
-  return res.send(`Bem-vindo, ${req.params.name}, por url_param`);
+  return res.send(
+    `Bem-vindo ao ${req.nameSystem}, ${req.params.name}, por url_param`
+  );
 });
 
 // query params /?nome_param=valor
@@ -16,4 +30,4 @@ app.get('/nome/', (req, res) => {
   return res.send(`Bem-vindo, ${req.query.name}, por query_param`);
 });
 
-app.listen(3002);
+app.listen(3000);
